a04f2e258864a7d289cbbb83ec062547b7ef8c95
Hila Serban <digitaldreams553@gmail.com>
2023-03-29 15:51:09 +0300

    Finalizing project

--- js/script.js
+++ js/script.js
@@ -30,3 +30,27 @@ window.onscroll = () => {
   menuIcon.classList.remove('bx-x');
   navbar.classList.remove('active');
 };
+
+/* scroll reveal */
+
+ScrollReveal({
+  reset: true,
+  distance: '80px',
+  duration: 2000,
+  delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .service-container, .portfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left'});
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right'});
+
+/* Typed js */
+
+const typed = new Typed('.multiple-text', {
+  strings: ['Frontend Developer', 'Youtuber', 'Blogger'],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true
+});
